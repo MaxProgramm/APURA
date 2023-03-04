b0c4fc62d8053fc6a9a3e216355d076a5c0f88c7
BuildTools <unconfigured@null.spigotmc.org>
2023-03-04 13:27:56 +0100

    v.5.0.0 Added Webinterface and live update functionality

--- testLiveUpdater.py
+++ testLiveUpdater.py
@@ -8,12 +8,12 @@ with open("config.json") as config_file:
 
 ip = json_data["server_ip"]
 
-counter = 0
+#counter = 0
 while True:
-    counter = counter + 1
-    with open("tempData.txt", "w") as file:
-        file.write(f"{counter}")
-        print(counter)
+    #counter = counter + 1
+    #with open("tempData.txt", "w") as file:
+    #    file.write(f"{counter}")
+    #    print(counter)
 
     data = f"2022.12.30 08:33:28 TEMPERATUR2 off 0 19.4 19.7"
     response = requests.post(f"http://{ip}:5000/message", data=data)
